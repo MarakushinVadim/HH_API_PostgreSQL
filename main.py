68547cd74b1582e5c5d250b2c4c4fb2a6c8cdff0
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-03-24 19:10:23 +0300

    add data

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from src.DBManager import DBManager
 from src.HH_API import HeadHunterAPI, Employer, Vacancy
 
 if __name__ == '__main__':
+
+    table = DBManager()
+    table.create_db()
+    table.create_tables()
+
 
     employers_id = ['3144945', '9280639', '68587', '1545815', '1729433', '4614421', '3127', '3776', '78638', '47858']
     for employer_id in employers_id:
@@ -10,33 +16,37 @@ if __name__ == '__main__':
         employer = Employer(emp['id'], emp['name'], emp['description'], emp['site_url'])
         vacancy_list = []
 
+        table.add_employer(employer)
 
 
-        print(employer.id, employer.name, employer.description, employer.site_url, sep='\n')
 
-        print(f'длинна {len(vac)}')
+        # print(employer.id, employer.name, employer.description, employer.site_url, sep='\n')
+        #
+        # print(f'длинна {len(vac)}')
 
-        for item in range(len(vac)):
-            if vac['items'][item]['salary'] is None:
-                vac['items'][item]['salary'] = {'from': None, 'to': None, 'currency': 'не указано', 'gross': True}
+
+
+        for item in vac['items']:
+            if item['salary'] is None:
+                item['salary'] = {'from': None, 'to': None, 'currency': 'не указано', 'gross': True}
             else:
                 pass
 
-        for item in range(len(vac)):
-            print(item)
-
+        for item in vac['items']:
             vacancy = Vacancy(
-                vac['items'][item]['id'],
-                vac['items'][item]['name'],
-                vac['items'][item]['area']['name'],
-                vac['items'][item]['salary']['from'],
-                vac['items'][item]['salary']['to'],
-                vac['items'][item]['salary']['currency'],
-                vac['items'][item]['type']['name'],  # тип вакансии(открытая/закрытая)(t_name)
-                vac['items'][item]['published_at'],
-                vac['items'][item]['alternate_url']
+                item['id'],
+                item['name'],
+                item['area']['name'],
+                item['salary']['from'],
+                item['salary']['to'],
+                item['salary']['currency'],
+                item['type']['name'],  # тип вакансии(открытая/закрытая)(t_name)
+                item['published_at'],
+                item['alternate_url']
             )
+
             vacancy_list.append(vacancy)
             print(vacancy.id, vacancy.name, vacancy.area, vacancy.s_from, vacancy.s_to, vacancy.s_currency, vacancy.t_name, vacancy.published_at, vacancy.url)
+
 
         print(vacancy_list)
